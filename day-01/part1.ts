5550dd6a91e964edc33dfbe9411e61c4590a99db
Shane Donnelly <shane.donnelly@bentley.com>
2023-12-23 17:09:58 +0000

    Tweaked comment.

--- day-01/part1.ts
+++ day-01/part1.ts
@@ -5,9 +5,9 @@ const getFlagValueFromArgs = (flag: string, defaultValue: string | number) => {
   var flagValueFromArgs;
   const inputFlagIndex = process.argv.indexOf(flag);
 
-  // Checks for -f and if it has a value
+  // Checks for flag in argv and to see if a value is provided
   if (inputFlagIndex > -1 && process.argv.length >= inputFlagIndex + 1) {
-    // Retrieve the value after -f
+    // Retrieve the value after flag
     flagValueFromArgs = process.argv[inputFlagIndex + 1];
   }
   // If no value, return default.
