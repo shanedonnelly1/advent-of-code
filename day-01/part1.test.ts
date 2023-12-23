e95c78299cd04f9b0287190083eec1845b493707
Shane Donnelly <shane.donnelly@bentley.com>
2023-12-23 17:08:41 +0000

    Add in ts-jest plus 2 small tests for part1.

---
+++ day-01/part1.test.ts
@@ -0,0 +1,9 @@
+import { getCalibrationValueFromFile } from "./part1";
+
+test("Calibration value from part1.input.294.txt to equal 294", async () => {
+  expect(await getCalibrationValueFromFile("./part1.input.294.txt")).toBe(294);
+});
+
+test("Calibration value from part1.input.142.txt to equal 142", async () => {
+  expect(await getCalibrationValueFromFile("./part1.input.142.txt")).toBe(142);
+});
