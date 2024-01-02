70059c02a770cd1bbae654af19fa3f24c1e2b57a
Shane Donnelly <shane.donnelly@bentley.com>
2024-01-02 22:21:30 +0000

    Added functions for getPartNumberSumForLine and getMatchPairArrayForLine. Added test for example input.

--- day-03/part1.test.ts
+++ day-03/part1.test.ts
@@ -1,7 +1,18 @@
-import { getProcessedValueFromFile, isPartNumber } from "./part1";
+import {
+  getMatchPairArrayForLine,
+  getPartNumberSumForLine,
+  getProcessedValueFromFile,
+  isPartNumber,
+} from "./part1";
 
-test("Processed value from input.txt to equal 0", async () => {
-  expect(await getProcessedValueFromFile("./day-03/data/input.txt")).toBe(0);
+// test("Processed value from input.txt to equal 0", async () => {
+//   expect(await getProcessedValueFromFile("./day-03/data/input.txt")).toBe(0);
+// });
+
+test("Processed value from part1.input.4361.txt to equal 4361", async () => {
+  expect(
+    await getProcessedValueFromFile("./day-03/data/part1.input.4361.txt")
+  ).toBe(4361);
 });
 
 test("Values that are part numbers", () => {
@@ -33,6 +44,7 @@ test("Values that are part numbers", () => {
     // Next line being undefined, but previous matches diagonally after
     isPartNumber("35", 2, "....*.....", undefined)
   ).toBeTruthy();
+  // TODO Test for part numbers with symobls on the same line
 });
 
 test("Values that are not a part numbers", () => {
@@ -45,4 +57,24 @@ test("Values that are not a part numbers", () => {
     // Next line being undefined
     isPartNumber("35", 2, "..........", undefined)
   ).toBeFalsy();
+});
+
+test("Get possible numbers for line", () => {
+  expect(getMatchPairArrayForLine("..35..633.")).toStrictEqual([
+    { index: 2, string: "35" },
+    { index: 6, string: "633" },
+  ]);
+  expect(getMatchPairArrayForLine("467..114..")).toStrictEqual([
+    { index: 0, string: "467" },
+    { index: 5, string: "114" },
+  ]);
+});
+
+test("Get part number sum for line", () => {
+  expect(
+    getPartNumberSumForLine("..35..633.", "...*......", "......#...")
+  ).toBe(668);
+  expect(getPartNumberSumForLine("467..114..", undefined, "...*......")).toBe(
+    467
+  );
 });
--- day-03/part1.ts
+++ day-03/part1.ts
@@ -28,9 +28,38 @@ export const isPartNumber = (
   return false;
 };
 
-export const getProcessedValueFromLine = (line: string, debug?: boolean) => {
+type MatchPair = {
+  index: number;
+  string: string;
+};
+
+export const getMatchPairArrayForLine = (line: string) => {
+  const regEx = /\d+/g;
+  const matches = line.matchAll(regEx);
+  var matchArray: MatchPair[] = [];
+  for (const match of matches) {
+    if (match.index !== undefined && match.length > 0) {
+      matchArray.push({ index: match.index, string: match[0] });
+    }
+  }
+  return matchArray;
+};
+
+export const getPartNumberSumForLine = (
+  line: string,
+  previousLine?: string,
+  nextLine?: string,
+  debug?: boolean
+) => {
   log(`${line}`, debug);
-  return 0;
+  var sum = 0;
+  const matchPairs = getMatchPairArrayForLine(line);
+  matchPairs.forEach((pair) => {
+    if (isPartNumber(pair.string, pair.index, previousLine, nextLine)) {
+      sum += Number(pair.string);
+    }
+  });
+  return sum;
 };
 
 export const getProcessedValueFromFile = async (
@@ -39,8 +68,27 @@ export const getProcessedValueFromFile = async (
 ) => {
   var sum = 0;
   const fileHandle = await open(file);
+  var lines: string[] = [];
   for await (const line of fileHandle.readLines()) {
-    const value = getProcessedValueFromLine(line, debug);
+    lines.push(line);
+    // const value = getPartNumberSumForLine(line, previousLine, nextLine, debug);
+    // log(`  Line value ${value}`, debug);
+    // sum += value;
+  }
+  for (var i = 0; i < lines.length; i += 1) {
+    var value = 0;
+    if (i === 0) {
+      value = getPartNumberSumForLine(lines[0], undefined, lines[1], debug);
+    } else if (i === lines.length - 1) {
+      value = getPartNumberSumForLine(lines[i], lines[i - 1], undefined, debug);
+    } else {
+      value = getPartNumberSumForLine(
+        lines[i],
+        lines[i - 1],
+        lines[i + 1],
+        debug
+      );
+    }
     log(`  Line value ${value}`, debug);
     sum += value;
   }
